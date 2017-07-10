e87dd184a310fab5b1c586e8a3c24e7c17b0286b
ValadeAurelien <valade.aurelien69580@gmail.com>
2017-07-10 13:20:48 +0200

    new host informations

--- includes/application.h
+++ includes/application.h
@@ -36,7 +36,7 @@ struct SocketStateName
                               "ConnectingStat",
                               "ConnectedState",
                               "BoundState",
-                              "ClosingState	",
+                              "ClosingState",
                               "ListeningState"};
 };
 
